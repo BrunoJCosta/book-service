c68cf7917c54c1948c0e7120c962ee733d3003dc
BrunoJCosta <brunojereissati00@gmail.com>
2025-03-18 13:08:06 -0300

    feat(book): configuration flyway

--- src/main/resources/db/migration/V1__Create_Table_Books.sql
+++ src/main/resources/db/migration/V1__Create_Table_Books.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE book (
   id bigserial PRIMARY KEY,
   author text,
-  launch_date timestamp(6) NOT NULL,
+  date timestamp(6) NOT NULL,
   price decimal(65,2) NOT NULL,
   title text
-)
+);
