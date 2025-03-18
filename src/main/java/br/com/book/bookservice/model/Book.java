0b2671058127512bbc6415c5887fd35d3179370a
Bruno <brunojereissati@edu.unifor.br>
2025-03-18 00:05:31 -0300

    feat(flyway) configuration wix

--- src/main/java/br/com/book/bookservice/model/Book.java
+++ src/main/java/br/com/book/bookservice/model/Book.java
@@ -1,6 +1,10 @@
 package br.com.book.bookservice.model;
 
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,13 +17,23 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Book implements Serializable {
 
+    @Id
+    @Column(name = "id")
     private Long id;
+    @Column(name = "title")
     private String title;
+    @Column(name = "author")
     private String author;
+    @Column(name = "date")
     private LocalDate date;
+    @Column(name = "price")
     private Double price;
+
+    @Transient
     private String currency;
+    @Transient
     private String environment;
 }
