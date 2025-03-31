139e6b395332a7157d9fd6745e4b4eb21e39e53a
BrunoJCosta <brunojereissati00@gmail.com>
2025-03-31 12:48:41 -0300

    feat(book): created the test to findByIdAndCurrency

---
+++ src/test/resources/db/migration/V1__Create_Table_Books.sql
@@ -0,0 +1,8 @@
+﻿create schema if not exists book_server;
+CREATE TABLE book_server.book (
+  id bigserial PRIMARY KEY,
+  author text,
+  date timestamp(6) NOT NULL,
+  price decimal(65,2) NOT NULL,
+  title text
+);
